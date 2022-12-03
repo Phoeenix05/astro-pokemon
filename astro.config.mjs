62f9f35c1bd8ad669ed5a9087523dd37a248b67c
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2022-12-03 18:50:04 +0200

    Added `@astrojs/vercel@2.3.4`

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()]
+  integrations: [solidJs(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
